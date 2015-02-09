070dbd6c9839ce7624239d80e1e05efdb6f506fc
Varad Meru <varad.meru@gmail.com>
2015-02-08 19:50:23 -0800

    First batch of questions done on Hackerrank! Liking it :)

---
+++ hello-scala/src/hackerrank/functionalprogramming/introduction/HelloWorldN.scala
@@ -0,0 +1,9 @@
+package hackerrank.functionalprogramming.introduction
+
+object HelloWorldN {
+  def main(args: Array[String]): Unit = {
+    for (a <- 1 to 4) {
+      println("Hello World")
+    }
+  }
+}
